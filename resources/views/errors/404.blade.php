98a20cc54b391b20cad74a330bc7d5d2d4a91918
difka09 <adifka09@gmail.com>
2019-05-09 22:25:17 +0700

    add 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<title>Bimko - 404 Halaman Tidak Ditemukan </title>
+
+	<!-- Required meta tags always come first -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" type="text/css" href="{{asset('guru/Bootstrap/dist/css/bootstrap-reboot.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('guru/Bootstrap/dist/css/bootstrap.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('guru/Bootstrap/dist/css/bootstrap-grid.css')}}">
+
+	<!-- Main Styles CSS -->
+	<link rel="stylesheet" type="text/css" href="{{asset('guru/css/main.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('guru/css/fonts.min.css')}}">
+
+	<!-- Main Font -->
+	<script src="{{asset('guru/js/webfontloader.min.js')}}"></script>
+	<script>
+		WebFont.load({
+			google: {
+				families: ['Roboto:300,400,500,700:latin']
+			}
+		});
+	</script>
+
+</head>
+<body class="body-bg-white">
+
+
+
+<!-- Stunning header -->
+
+<div class="stunning-header bg-primary-opacity">
+
+
+
+	<div class="header-spacer--standard"></div>
+
+	<div class="stunning-header-content">
+		<h1 class="stunning-header-title">404 Halaman Tidak Ditemukan</h1>
+		<ul class="breadcrumbs">
+			<li class="breadcrumbs-item">
+				<a href="#">Home</a>
+				<span class="icon breadcrumbs-custom">/</span>
+			</li>
+			<li class="breadcrumbs-item active">
+				<span>404 Error</span>
+			</li>
+		</ul>
+	</div>
+
+	<div class="content-bg-wrap stunning-header-bg1"></div>
+</div>
+
+<!-- End Stunning header -->
+
+
+<section class="medium-padding120">
+	<div class="container">
+		<div class="row">
+			<div class="col col-xl-6 m-auto col-lg-6 col-md-12 col-sm-12 col-12">
+				<div class="page-404-content">
+					<img src="{{asset('guru/img/404.png')}}" alt="photo">
+					<div class="crumina-module crumina-heading align-center">
+						<h2 class="h1 heading-title">A <span class="c-primary">Bimko Developer</span> Maaf, Halaman yang anda cari tidak ditemukan</h2>
+                        <p class="heading-text">Mungkin konten yang anda cari telah dihapus silahkan kembali ke <a href="{{Route('guru.index')}}">beranda</a>
+						</p>
+					</div>
+
+					<a href="{{Route('guru.index')}}" class="btn btn-primary btn-lg">Kembali Ke Beranda</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+
+
+<!-- Section Call To Action Animation -->
+
+<section class="align-right pt160 pb80 section-move-bg call-to-action-animation scrollme">
+
+	<div class="content-bg-wrap bg-section1"></div>
+</section>
+
+<!-- ... end Section Call To Action Animation -->
+
+
+<a class="back-to-top" href="#">
+	<img src="{{asset('guru/svg-icons/back-to-top.svg')}}" alt="arrow" class="back-icon">
+</a>
+
+
+<!-- JS Scripts -->
+<script src="{{asset('guru/js/jquery-3.2.1.js')}}"></script>
+<script src="{{asset('guru/js/jquery.appear.js')}}"></script>
+<script src="{{asset('guru/js/jquery.mousewheel.js')}}"></script>
+<script src="{{asset('guru/js/perfect-scrollbar.js')}}"></script>
+<script src="{{asset('guru/js/jquery.matchHeight.js')}}"></script>
+<script src="{{asset('guru/js/svgxuse.js')}}"></script>
+<script src="{{asset('guru/js/imagesloaded.pkgd.js')}}"></script>
+<script src="{{asset('guru/js/Headroom.js')}}"></script>
+<script src="{{asset('guru/js/velocity.js')}}"></script>
+<script src="{{asset('guru/js/ScrollMagic.js')}}"></script>
+<script src="{{asset('guru/js/jquery.waypoints.js')}}"></script>
+<script src="{{asset('guru/js/jquery.countTo.js')}}"></script>
+<script src="{{asset('guru/js/popper.min.js')}}"></script>
+<script src="{{asset('guru/js/material.min.js')}}"></script>
+<script src="{{asset('guru/js/bootstrap-select.js')}}"></script>
+<script src="{{asset('guru/js/smooth-scroll.js')}}"></script>
+<script src="{{asset('guru/js/selectize.js')}}"></script>
+<script src="{{asset('guru/js/swiper.jquery.js')}}"></script>
+<script src="{{asset('guru/js/moment.js')}}"></script>
+<script src="{{asset('guru/js/daterangepicker.js')}}"></script>
+<script src="{{asset('guru/js/simplecalendar.js')}}"></script>
+<script src="{{asset('guru/js/fullcalendar.js')}}"></script>
+<script src="{{asset('guru/js/isotope.pkgd.js')}}"></script>
+<script src="{{asset('guru/js/ajax-pagination.js')}}"></script>
+<script src="{{asset('guru/js/Chart.js')}}"></script>
+<script src="{{asset('guru/js/chartjs-plugin-deferred.js')}}"></script>
+<script src="{{asset('guru/js/circle-progress.js')}}"></script>
+<script src="{{asset('guru/js/loader.js')}}"></script>
+<script src="{{asset('guru/js/run-chart.js')}}"></script>
+<script src="{{asset('guru/js/jquery.magnific-popup.js')}}"></script>
+<script src="{{asset('guru/js/jquery.gifplayer.js')}}"></script>
+<script src="{{asset('guru/js/mediaelement-and-player.js')}}"></script>
+<script src="{{asset('guru/js/mediaelement-playlist-plugin.min.js')}}"></script>
+
+<script src="{{asset('guru/js/base-init.js')}}"></script>
+<script defer src="{{asset('guru/fonts/fontawesome-all.js')}}"></script>
+
+<script src="{{asset('guru/Bootstrap/dist/js/bootstrap.bundle.js')}}"></script>
+
+</body>
+</html>
